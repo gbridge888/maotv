c4b79a940882fc9d925e723d259d48407f8b2322
test <test@test.com>
2022-10-25 13:05:55 -0700

    u

---
+++ JS/js/Auete.js
@@ -0,0 +1,9 @@
+var rule = Object.assign(muban.mxpro,{
+title:'Auete',
+// host:'https://www.555dy.app',
+host:'https://www.auete.org/',
+headers:{//网站的请求头,完整支持所有的,常带ua和cookies
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+//   "Cookie": "searchneed=ok"
+},
+});
---
+++ JS/js/稀飯.js
@@ -0,0 +1,4 @@
+var rule = Object.assign(muban.首图,{
+title:'稀飯',
+host:'https://www.xifanys.com',
+});
